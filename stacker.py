c61a9eb97646b950f9955cac9b31bb34e6bc21b2
Kyle Shaffer <kyle.james.shaffer@gmail.com>
2015-12-23 12:42:17 -0500

    Update stacker.py
    Formatting code and comments.

--- stacker.py
+++ stacker.py
@@ -1,6 +1,10 @@
 """
-Example Code for Stacking Idea
+Code for Stacking Idea
+Author: Kyle Shaffer
+Competition: Kaggle - What's Cooking?
 """
+
+# Import relevant libraries
 from __future__ import division
 from datetime import datetime
 from pprint import pprint
@@ -12,12 +16,19 @@ from sklearn.ensemble import BaggingClassifier, RandomForestClassifier, ExtraTre
 from sklearn.naive_bayes import BernoulliNB
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression, SGDClassifier
-from sklearn.cross_validation import StratifiedKFold, cross_val_score
+from sklearn.cross_validation import StratifiedKFold
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_selection import chi2, SelectKBest
 from sklearn.grid_search import GridSearchCV
 
-
+# Set initial classifiers.
+# Each of these classifiers will output class probabilities
+# for each of the cuisine classes. Given that there are 6 classifiers,
+# the final dataset will consist of 120 (6 * 20 classes) features.
+# The logistic regression classifier below will be trained on these
+# class probabilities.
+# Hyper-parameters for each of the component classifiers were arrived
+# at through experimentation on the TFIDF and recipe-length features.
 clfs = [LogisticRegression(C=10, class_weight='balanced'),
         BernoulliNB(alpha=0.1),
         BaggingClassifier(base_estimator=LogisticRegression(C=10), n_estimators=100),
@@ -25,6 +36,7 @@ clfs = [LogisticRegression(C=10, class_weight='balanced'),
         RandomForestClassifier(n_estimators=200, max_features='auto', class_weight='balanced'),
         ExtraTreesClassifier(n_estimators=200)]
 
+# Helper function to load data quickly.
 def load_data():
     # Read in data
     with open('train.json', 'r') as infile:
@@ -47,7 +59,8 @@ def load_data():
     test_lengths = test_lengths.reshape(len(test_ingredients), 1)
     return train_ingred_strings, train_lengths, y, test_ingred_strings, test_lengths, labels_dict
 
-    
+# Function to quickly extract features.
+# These features were largely arrived at through experimentation.
 def extract_features(train_strings, train_lengths, labels, test_strings, test_lengths, feature_select=True):
     # TFIDF training features
     tfidfvec = TfidfVectorizer(ngram_range=(1,2), stop_words='english', analyzer='word')
@@ -65,6 +78,8 @@ def extract_features(train_strings, train_lengths, labels, test_strings, test_le
     test_features = sp.sparse.hstack((test_tfidf, test_lengths)).tocsr()
     return train_features, labels, test_features
 
+# Function for generating the train and testing dataset for the final Logistic Regression
+# classifier near the end.
 def run_stacker(clfs):
     np.random.seed(0) # seed to shuffle the train set
 
@@ -112,38 +127,32 @@ def run_stacker(clfs):
             dataset_blend_test_j += clf.predict_proba(X_submission)
         dataset_blend_test[:, row_cnt: row_cnt+20] = dataset_blend_test_j / 10.
         row_cnt += 20
-        """if j==0:
-            dataset_blend_test[:, 0:20] = dataset_blend_test_j / 10.
-        elif j==1:
-            dataset_blend_test[:, 20:40] = dataset_blend_test_j / 10.
-        elif j==2:
-            dataset_blend_test[:, 40:60] = dataset_blend_test_j / 10.
-        elif j==3:
-            dataset_blend_test[:, 60:80] = dataset_blend_test_j / 10.
-        else:
-            dataset_blend_test[:, 80:] = dataset_blend_test_j / 10."""
-
     print
-    print "Blending."
+    print "Training Logistic Regression classifier."
+    # C parameter here set through experimentation.
     clf = LogisticRegression(C=10)
     clf.fit(dataset_blend_train, y)
+    # Outputting an initial estimate of performance for the stacking model.
     print "CV 5 Score:", cross_val_score(clf, X=dataset_blend_train, y=y, cv=5).sum()/5.
-    
+    # Output the training set, test set, and labels for final submission.
     return dataset_blend_train, y, dataset_blend_test, X, X_submission
 
+# Function for running final predictions. 
+# This is mostly to tune the final hyper-parameters
+# for the Logistic Regression stacker.
 def run_final_predictions(X_train, y_train, X_submit, clf):
     param_grid = {'C': [0.001, 0.01, 0.1, 1, 7, 10], 'class_weight': [None, 'balanced']}
     grid = GridSearchCV(estimator=clf, param_grid=param_grid, cv=10)
     grid.fit(X_train, y_train)
     print grid.best_estimator_
-    #clf.fit(X_train, y_train)
-    
+
     predictions = grid.best_estimator_.predict(X_submit)
     
     with open('test.json', 'r') as infile:
         test = json.load(infile)
     ids = [i['id'] for i in test]
     
+    # Format classes for final output
     label_map = load_data()[-1]
     vals = label_map.values()
     rev_labels_dict = dict(zip(vals, label_map.keys()))
@@ -155,7 +164,9 @@ def run_final_predictions(X_train, y_train, X_submit, clf):
     output.to_csv('{0}.csv'.format(datetime.now()), index=False)
 
 
-# Run it
+# Run all the code to generate final output.
+# Save features generated from base classifiers along the way.
+# This was mostly to facilitate additional experimentation.
 if __name__ == "__main__":
     X_train_blend, y_train, X_submit_blend, X_train_base, X_submit_base = run_stacker(clfs)
     with open('X_train_base.pickle', 'w') as outfile:
@@ -167,11 +178,5 @@ if __name__ == "__main__":
     with open('X_submit_base.pickle', 'w') as outfile:
         cPickle.dump(X_submit_base, outfile)
     run_final_predictions(X_train_blend, y_train, X_submit_blend, LogisticRegression())
-
-### Testing two additional classifiers
-#from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis, LinearDiscriminantAnalysis
-#qda = QuadraticDiscriminantAnalysis()
-#lda = LinearDiscriminantAnalysis()
-#print cross_val_score(lda, X=base_features.toarray(), y=y, cv=5).sum() / 5
 
 
